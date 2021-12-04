b00ff030267cf9234cb0072255e93ebf532c7736
Emile Hreich <emile.jd.hreich@gmail.com>
2021-12-04 11:52:35 +0100

    ClientServerTCP

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -38,98 +38,106 @@ public class TCPClient {
     
     }
     
+    private static void sendBytes(FileInputStream fis, OutputStream os) throws IOException {
+        // Construct a 1K buffer to hold bytes on their way to the socket.
+        byte[] buffer = new byte[1024];
+        int bytes = 0;
+
+        // Copy requested file into the socket's output stream.
+        while ((bytes = fis.read(buffer)) != -1) {
+            os.write(buffer, 0, bytes);
+        }
+    }
+
     private static boolean sendFile(DataOutputStream os, String filename) throws IOException{
 
         boolean retval;
 
         if (filename.isEmpty()) {
             retval = false;
-            
+            os.writeInt(0);
         } else {
             retval = true;
+
+            // Send the file length
+            int length = getFileLength(filename);
+            System.out.println("The file has length: " + length + " bytes");
+            os.writeInt(length);
+
             // Send the file itself
             FileInputStream fis = getFileReader(filename);
-            if (fis != null){
-                int length = getFileLength(filename);
-                byte[] content = fis.readNBytes(length);
-                os.write(content);
-
-                System.out.println("sent");
-//                retval = length == transferred;
-            }else{
-                retval = false;
-            }
+            sendBytes(fis, os);
         }
             
         return retval;
     }
     private static void handleResponse(DataInputStream inFromServer) throws IOException {
         
-//        int num_values = 5;
-//        System.out.println("There are " + num_values + " unique words in the document,→ \n");
-//        for (int i = 0; i < num_values; i++) {
-//            // Read the length of the word
-//            int length = inFromServer.
-//            // Allocate a big enough buffer for the word
-//            byte[] bytearray = new byte[length];
-//            // Actually read the word and convert it to a string
-//
-//            String word = new String(bytearray);
-//            // Read the number of occurrences
-//            int times = ...
-//            System.out.println(word + ": " + times);
-//        }
-        assert inFromServer != null;
-        byte[] in = inFromServer.readAllBytes();
-        System.out.println(new String(in));
+        int num_values = inFromServer.readInt();
 
+        System.out.println("There are " + num_values + " unique words in the document \n");
 
+        for (int i = 0; i < num_values; i++) {
+            // Read the length of the word
+            int length = inFromServer.readInt();
+
+            // Allocate a big enough buffer for the word
+            byte[] bytearray = new byte[length];
+
+            // Actually read the word and convert it to a string
+            inFromServer.readFully(bytearray);
+            String word = new String(bytearray);
+
+            // Read the number of occurrences
+            int times = inFromServer.readInt();
+
+            System.out.println(word + ": " + times);
+        }
     }
 
     public static void main(String argv[]) throws IOException {
 
         Socket clientSocket = null;
+
         BufferedReader inFromUser = null;
         DataOutputStream outToServer = null;
         DataInputStream inFromServer = null;
+
         Boolean repeatFlag;
 
         try {
             // Connect to the local server at 6789
             int port = 6789;
-            clientSocket = new Socket("localhost",port);
+            clientSocket = new Socket("localhost", port);
             inFromUser = new BufferedReader(new InputStreamReader(System.in));
             outToServer = new DataOutputStream(clientSocket.getOutputStream());
             inFromServer = new DataInputStream(clientSocket.getInputStream());
-            System.out.println("Connected to server");
+            do{
+                System.out.print("Enter a file name: ");
+                String filename = inFromUser.readLine();
 
-            System.out.print("Enter a file name: ");
-            String filename = inFromUser.readLine();
-            sendFile(outToServer, filename);
-            handleResponse(inFromServer);
-//            do {
-//                System.out.print("Enter a file name: ");
-//                String filename = inFromUser.readLine();
-//
-//                // sendfile will notify us whether this is the final file or not
-//                repeatFlag = sendFile(outToServer, filename);
-//                if (repeatFlag == true) {
-//                    // If we didn't send a file,
-//                    // we don't need to wait for a response
-//                    handleResponse(inFromServer);
-//            }while(repeatFlag == true);
-//
+                // sendfile will notify us whether this is the final file or not
+                repeatFlag = sendFile(outToServer, filename);
+                if (repeatFlag == true) {
+                    // If we didn't send a file,
+                    // we don't need to wait for a response
+                    handleResponse(inFromServer);
+                }
+
+            } while(repeatFlag == true);
+
+
         } catch (IOException ioex) {
             System.out.println("Failed to process request : " + ioex.getMessage());
         } finally {
-            //Close all input/output/sockets
-            assert inFromServer != null;
-            inFromServer.close();
-            inFromUser.close();
-            outToServer.close();
+            // Close all input/output/sockets
+            try { if(outToServer != null) outToServer.close(); } catch(IOException e) {}
+            try { if(inFromServer != null) inFromServer.close(); } catch(IOException e) {}
+            try { if(inFromUser != null) inFromUser.close(); } catch(IOException e) {}
+            try { if(clientSocket != null) clientSocket.close(); } catch(IOException e) {}
         }
 
     }
-//    }
+
 
 }
--- src/TCPServer.java
+++ src/TCPServer.java
@@ -39,8 +39,18 @@ public class TCPServer {
 
         for (String key: occurrences.keySet()) {
             String word = key.toString();
-            String times = occurrences.get(key).toString();
-            response = response.concat(word + ": " + times + "\n");
+            int times = occurrences.get(key);
+
+            // Send the length of the word first
+            int length = word.length();
+            out.writeInt(length);
+
+            // Then, send the actual word
+            byte[] b = word.getBytes();
+            out.write(b, 0, length);
+
+            // Finally, send the number of times the word appears
+            out.writeInt(times);
         }
         out.writeBytes(response);
 
@@ -55,33 +65,80 @@ public class TCPServer {
 
             // Open the input-output streams
             try{
-                System.out.println("debug: opening streams");
+
+                // Set reads to timeout after 50 seconds (50000 milliseconds)
+                connectionSocket.setSoTimeout(50000);
 
                 inFromClient = new DataInputStream(connectionSocket.getInputStream());
                 outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+
+                // This variable controls when the loop should terminate
+                boolean repeatFlag = true;
+
+                do {
+                    System.out.println("Waiting to receive a file or close after 50 seconds...");
+    
+                    // Read the length of the file
+                    int length = inFromClient.readInt();
+                    System.out.println("The file has length: " + length + " bytes");
+    
+                    if (length == 0) {
+                        // Terminate the connection
+                        repeatFlag = false;
+                    } else {
+                        // Read the file contents into message
+                        byte[] bytearray = new byte[length];
+                        inFromClient.readFully(bytearray);
+                        String message = new String(bytearray);
+                        System.out.println(message);
+    
+                        // Call the response handler
+                        send_response(outToClient, message);
+                    }
+                } while (repeatFlag == true);
+
             }catch(IOException s){
                 System.out.println("I/O Error");
-            }
-            // Read the file contents into message
-            assert inFromClient != null;
-            byte[] bytearray = inFromClient.readAllBytes();
+            }finally{
+                // Close all input/output/sockets
+                // The stream-closing operations need to be nested in try-catch blocks
+                // Solution inspired by:
+                // http://javarevisited.blogspot.ch/2014/10/right-way-to-close-inputstream-file-resource-in-java.html
 
-            String message = new String(bytearray);
-
-            // Call the response handler
-            send_response(outToClient, message);
-//        inFromClient.close();
-//        outToClient.close();
-
+                try { if (outToClient != null) outToClient.close();
+                } catch(IOException e) {}
+                try { if (inFromClient != null) inFromClient.close();
+                } catch(IOException e) {}
+                try { if (connectionSocket != null) connectionSocket.close();
+                } catch(IOException e) {}
+             }
 
     }
     
-    public static void main(String args[]) throws IOException {
-    //@TODO
-        while (true){
-            
+    public static void main(String args[]) {
+        ServerSocket welcomeSocket = null;
+        Socket connectionSocket = null;
+
+        try {
+            // Create a socket that listens to port 6789
+            welcomeSocket = new ServerSocket(6789);
+
+            while(true) {
+                try {
+                    // Get a new connection
+                    System.out.println("Waiting for a new client...");
+                    connectionSocket = welcomeSocket.accept();
+                    
+                    // pass the connection socket to the handler
+                    System.out.println("Handling new client...");
+                    handleConnection(connectionSocket);
+                } catch (IOException ioex) {}
+            }
+        } catch (IOException ioex) {
+            System.out.println("Failed to open welcomeSocket : " + ioex.getMessage());
+        } finally {
+            try { if (welcomeSocket != null) welcomeSocket.close(); } catch(IOException e) {}
         }
-
-
     }
 }
